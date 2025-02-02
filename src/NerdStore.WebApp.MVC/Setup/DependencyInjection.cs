6eafba50227dc56e146f50bd77062b03b2579137
Eduardo <Eduardo@AVELL>
2025-02-02 10:21:53 -0300

    Add project files.

---
+++ src/NerdStore.WebApp.MVC/Setup/DependencyInjection.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using NerdStore.Catalogo.Application.Services;
+using NerdStore.Catalogo.Data;
+using NerdStore.Catalogo.Data.Repository;
+using NerdStore.Catalogo.Domain;
+using NerdStore.Catalogo.Domain.Events;
+using NerdStore.Core.Bus;
+using NerdStore.Vendas.Application.Commands;
+
+namespace NerdStore.WebApp.MVC.Setup;
+
+public static class DependencyInjection
+{
+    public static void RegisterServices(this IServiceCollection services)
+    {
+        // Mediator
+        services.AddScoped<IMediatrHandler, MediatrHandler>();
+
+        // Catalogo
+        services.AddScoped<IProdutoRepository, ProdutoRepository>();
+        services.AddScoped<IProdutoAppService, ProdutoAppService>();
+        services.AddScoped<IEstoqueService, EstoqueService>();
+        services.AddScoped<CatalogoContext>();
+
+        services.AddScoped<INotificationHandler<ProdutoAbaixoEstoqueEvent>, ProdutoEventHandler>();
+
+        // Vendas
+        services.AddScoped<IRequestHandler<AdicionarItemPedidoCommand, bool>, PedidoCommandHandler>();
+    }
+}
