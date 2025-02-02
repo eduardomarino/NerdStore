2109ffcfe5a82298b06937fee01b9177406f6055
Eduardo <Eduardo@AVELL>
2025-02-02 10:21:53 -0300

    Add project files.

---
+++ src/NerdStore.Core/DomainObjects/DomainException.cs
@@ -0,0 +1,14 @@
+﻿namespace NerdStore.Core.DomainObjects;
+
+public class DomainException : Exception
+{
+    public DomainException()
+    { }
+
+    public DomainException(string message) : base(message)
+    { }
+
+    // innerException para especializar a exceção
+    public DomainException(string message, Exception innerException) : base(message, innerException)
+    { }
+}
---
+++ src/NerdStore.WebApp.MVC/Setup/DependencyInjection.cs
@@ -0,0 +1,44 @@
+﻿using MediatR;
+using NerdStore.Catalogo.Application.Services;
+using NerdStore.Catalogo.Data;
+using NerdStore.Catalogo.Data.Repository;
+using NerdStore.Catalogo.Domain;
+using NerdStore.Catalogo.Domain.Events;
+using NerdStore.Core.Communication.Mediator;
+using NerdStore.Core.Messages.CommonMessages.Notifications;
+using NerdStore.Vendas.Application.Commands;
+using NerdStore.Vendas.Application.Events;
+using NerdStore.Vendas.Data;
+using NerdStore.Vendas.Data.Repository;
+using NerdStore.Vendas.Domain;
+
+namespace NerdStore.WebApp.MVC.Setup;
+
+public static class DependencyInjection
+{
+    public static void RegisterServices(this IServiceCollection services)
+    {
+        // Mediator
+        services.AddScoped<IMediatorHandler, MediatorHandler>();
+
+        // Notifications
+        services.AddScoped<INotificationHandler<DomainNotification>, DomainNotificationHandler>();
+
+        // Catalogo
+        services.AddScoped<IProdutoRepository, ProdutoRepository>();
+        services.AddScoped<IProdutoAppService, ProdutoAppService>();
+        services.AddScoped<IEstoqueService, EstoqueService>();
+        services.AddScoped<CatalogoContext>();
+
+        services.AddScoped<INotificationHandler<ProdutoAbaixoEstoqueEvent>, ProdutoEventHandler>();
+
+        // Vendas
+        services.AddScoped<IRequestHandler<AdicionarItemPedidoCommand, bool>, PedidoCommandHandler>();
+        services.AddScoped<IPedidoRepository, PedidoRepository>();
+        services.AddScoped<VendasContext>();
+
+        services.AddScoped<INotificationHandler<PedidoRascunhoIniciadoEvent>, PedidoEventHandler>();
+        services.AddScoped<INotificationHandler<PedidoAtualizadoEvent>, PedidoEventHandler>();
+        services.AddScoped<INotificationHandler<PedidoItemAdicionadoEvent>, PedidoEventHandler>();
+    }
+}
